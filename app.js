c67d105043842dd3e3886e703303aa3ebfccd6ae
xyclr <xyclr@163.com>
2015-06-25 10:34:34 +0800

    aap.js
    weichat

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 80);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
 var wechat = require('wechat');
 var config = {
-  token: 'token',
-  appid: 'appid',
-  encodingAESKey: 'encodinAESKey'
+  token: 'weixin',
+  appid: 'wxd8ffa8619684a575',
+  encodingAESKey: '01HCbOCSsPxYbFFlOzSUAGZBIyEK5jYUn19g6H6psci'
 };
 
 app.use(express.query());
