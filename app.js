d09f2195b74410d66b992387a6531ecfce3f5d63
xyclr <xyclr@163.com>
2015-07-01 11:09:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('/user', function(req, res) {
         console.log(oauth_user.nickname);
     });
   });
-  api.getUserByCode(code, function (err, result) {
+  client.getUserByCode(code, function (err, result) {
     console.info("result :" + result);
   });
  
