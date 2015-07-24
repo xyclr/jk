bbec233c277abb5de8f29d745d071a9f0d13de22
xyclr <xyclr@163.com>
2015-07-24 20:55:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var request = require('request');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
