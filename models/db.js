204114e6ee8ef6a0bd5defa64de3d0281df91d10
xyclr <xyclr@163.com>
2015-08-08 21:18:35 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/bs');
+mongoose.connect('mongodb://xyclr:gjj123456@localhost/bs');
 mongoose.Obj = mongoose.ObjectID;
 module.exports = mongoose;
