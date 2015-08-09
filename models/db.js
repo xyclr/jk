847fbcfc148121c6f57429917444a3aa3197afaf
xyclr <xyclr@163.com>
2015-08-09 10:24:35 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://ndesig:gjj123456@localhost/bs');
+mongoose.connect('mongodb://ndesig:gjj123456@45.79.9.136:27017/bs');
 mongoose.Obj = mongoose.ObjectID;
 module.exports = mongoose;
